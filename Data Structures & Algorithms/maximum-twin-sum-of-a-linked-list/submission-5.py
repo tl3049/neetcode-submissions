2284ea27fda48272c44c5183009d3c18870e793d
Tong Liu <91927221+tl3049@users.noreply.github.com>
2026-09-07 17:14:30 -0400

    Add: maximum-twin-sum-of-a-linked-list - submission-5

---
+++ Data Structures & Algorithms/maximum-twin-sum-of-a-linked-list/submission-5.py
@@ -0,0 +1,70 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        # dummy = ListNode(-1)
+        # slow, fast = dummy, dummy
+        # dummy.next = head
+        # while fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+        
+        # stop = slow.next
+        # #Reverse link from head to slow
+        # prev = dummy
+        # cur = head
+        # while cur != stop:#过程中slow指针已经被改变所有slow.next要提前保存
+        #     tmp = cur.next
+        #     cur.next = prev
+        #     prev = cur
+        #     cur = tmp
+        
+        # res = 0
+        # while cur:
+        #     res = max(res, cur.val + prev.val)
+        #     cur = cur.next
+        #     prev = prev.next
+        # return res
+        
+        # slow, fast = head, head
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+        # #Reverse link from head to slow
+        # prev = None
+        # cur = head
+        # while cur != slow:
+        #     tmp = cur.next
+        #     cur.next = prev
+        #     prev = cur
+        #     cur = tmp
+        # res = 0
+        # while cur:
+        #     res = max(res, cur.val + prev.val)
+        #     cur = cur.next
+        #     prev = prev.next
+        # return res
+
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        #Reverse link from slow to end
+        prev = None
+        cur = slow
+        while cur:
+            tmp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = tmp
+        res = 0
+        cur = head
+        while prev:
+            res = max(res, cur.val + prev.val)
+            cur = cur.next
+            prev = prev.next
+        return res
+        
