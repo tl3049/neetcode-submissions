41fe2ce0df2ad5a2a7a28e4eaa4ea63f4a80673e
Tong Liu <91927221+tl3049@users.noreply.github.com>
2026-09-17 15:23:26 -0400

    Add: course-schedule-iv - submission-8

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        n = numCourses
+        adj = {i:[] for i in range(n)}
+        for a, b in prerequisites:
+            adj[a].append(b)
+        def dfs(i):
+            if i in visit:
+                return 
+            visit.add(i)
+            for j in adj[i]:
+                dfs(j)
+            topSort.add(i)
+            return 
+        res = []
+        for u, v in queries:
+            topSort = set()
+            visit = set()
+            dfs(u)
+            if v in topSort:
+                res.append(True)
+            else:
+                res.append(False)
+        return res
