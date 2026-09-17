18bef57fd4eb6530cfc5f9a21cb4a28e9c158772
Tong Liu <91927221+tl3049@users.noreply.github.com>
2026-09-17 14:17:59 -0400

    Add: topologicalSort - submission-9

---
+++ Data Structures & Algorithms/topologicalSort/submission-9.py
@@ -0,0 +1,29 @@
+class Solution:
+    def topologicalSort(self, n: int, edges: List[List[int]]) -> List[int]:
+        #Set adj list
+        adj = {i:[] for i in range(n)}
+        for u, v in edges:
+            adj[u].append(v)
+        #DFS for topological sort
+        topSort = []
+        visit = set()
+        path = set()
+        def dfs(i, topSort, visit, path):
+            if i in path:
+                return False
+            if i in visit:
+                return True
+            visit.add(i)
+            path.add(i)
+            for j in adj[i]:
+                if not dfs(j, topSort, visit, path):
+                    return False
+            topSort.append(i)
+            path.remove(i)
+            return True
+        #Iteration for all possible nodes
+        for i in range(n):
+            if not dfs(i, topSort, visit, path):
+                return []
+        topSort.reverse()
+        return topSort
