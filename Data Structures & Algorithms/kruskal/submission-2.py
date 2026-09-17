95da97f43b03d91254619b9fd26a87f76e20d14e
Tong Liu <91927221+tl3049@users.noreply.github.com>
2026-09-16 22:49:43 -0400

    Add: kruskal - submission-2

---
+++ Data Structures & Algorithms/kruskal/submission-2.py
@@ -0,0 +1,55 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par = {}
+        self.rank = {}
+        for i in range(n):
+            self.par[i] = i
+            self.rank[i] = 1
+    def find(self, n):
+        p = self.par[n]
+        while p != self.par[p]:
+            self.par[p] = self.par[self.par[p]]
+            p = self.par[p]
+        return p
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2:
+            return False
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+        elif self.rank[p2] > self.rank[p1]:
+            self.par[p1] = p2
+        else:
+            self.par[p2] = p1
+            self.rank[p1] +=1
+        return True
+import heapq
+class Solution:
+    def minimumSpanningTree(self, n: int, edges: List[List[int]]) -> int:
+        minH = []
+        for src, dst, weight in edges:
+            heapq.heappush(minH, [weight, src, dst])
+        uf = UnionFind(n)
+        total = 0
+        k = 0
+        while minH:
+            weight, src, dst = heapq.heappop(minH)
+            if not uf.union(src, dst):
+                continue
+            total += weight
+            k += 1
+            if k == n - 1:
+                return total
+        return -1
+
+
+
+
+
+
+
+
+
+
+
+
