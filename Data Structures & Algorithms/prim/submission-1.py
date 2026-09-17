a6c5afa372e38bdafe8829cff657337a1134eca4
Tong Liu <91927221+tl3049@users.noreply.github.com>
2026-09-16 21:13:48 -0400

    Add: prim - submission-1

---
+++ Data Structures & Algorithms/prim/submission-1.py
@@ -0,0 +1,51 @@
+import heapq
+class Solution:
+    def minimumSpanningTree(self, n: int, edges: List[List[int]]) -> int:
+        # adj = {}
+        # for i in range(n):
+        #     adj[i] = []
+        # for src, dst, weight in edges:
+        #     adj[src].append([weight, src, dst])
+        #     adj[dst].append([weight, dst, src])
+        # minH = []
+        # for weight, src, dst in adj[0]:
+        #     heapq.heappush(minH,[weight, src, dst])
+        # visit = set()
+        # visit.add(0)
+        # total = 0
+        # while minH:
+        #     weight, src, dst = heapq.heappop(minH)
+        #     if dst in visit:
+        #         continue
+        #     total += weight
+        #     visit.add(dst)
+        #     if len(visit) == n:
+        #         return total
+        #     for w, s, d in adj[dst]:
+        #         if d not in visit:
+        #             heapq.heappush(minH, [w, s, d])
+        # return -1
+
+        #SIMPLFIED VERSION
+        adj = {}
+        for i in range(n):
+            adj[i] = []
+        for src, dst, weight in edges:
+            adj[src].append([weight, dst])
+            adj[dst].append([weight, src])
+        minH = [[0, 0]]
+        visit = set()
+        total = 0
+        while minH:
+            weight, dst = heapq.heappop(minH)
+            if dst in visit:
+                continue
+            total += weight
+            visit.add(dst)
+            if len(visit) == n:
+                return total
+            for w, d in adj[dst]:
+                if d not in visit:
+                    heapq.heappush(minH, [w, d])
+        return -1
+
