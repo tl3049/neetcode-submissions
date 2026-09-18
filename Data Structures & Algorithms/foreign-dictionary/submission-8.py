d54cb652835e86f39d9a82fca5524101e9d403dd
Tong Liu <91927221+tl3049@users.noreply.github.com>
2026-09-17 21:24:04 -0400

    Add: foreign-dictionary - submission-8

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-8.py
@@ -0,0 +1,54 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        adj = {c:set() for word in words for c in word}
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i+1]
+            minLen = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+                return ""
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    adj[w1[j]].add(w2[j])
+                    break
+        visited = {}
+        res = []
+
+        def dfs(char):
+            if char in visited:
+                return visited[char]
+
+            visited[char] = True
+
+            for neighChar in adj[char]:
+                if dfs(neighChar):
+                    return True
+
+            visited[char] = False
+            res.append(char)
+
+        for char in adj:
+            if dfs(char):
+                return ""
+
+        res.reverse()
+        return "".join(res)
+        # visit, path = set(), set()
+        # topSort = []
+        # def dfs(c):
+        #     if c in path:
+        #         return False
+        #     if c in visit:
+        #         return True
+        #     visit.add(c)
+        #     path.add(c)
+        #     for d in adj[c]:
+        #         if not dfs(d):
+        #             return False
+        #     topSort.append(c)
+        #     path.remove(c)
+        #     return True
+        # for c in adj:
+        #     if not dfs(c):
+        #         return ""
+        # topSort.reverse()
+        # return "".join(topSort)
